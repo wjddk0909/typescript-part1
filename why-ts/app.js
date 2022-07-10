a390b64ee3e1c51f645359c7536ff0344ef051a0
wjddk0909 <fjddkcb@naver.com>
2022-07-10 18:20:08 +0900

    왜 자바스크립트를 쓰면 좋은가요?

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,5 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = 'https://jsonplaceholder.typicode.com/users';
 
 // dom
 var username = document.querySelector('#username');
@@ -13,9 +13,13 @@ function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user)
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street;
     })
     .catch(function (error) {
       console.log(error);
