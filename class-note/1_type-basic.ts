f9c7b10688ecc57c2ceb078dcd0e7f2a2ac87c6b
wjddk0909 <fjddkcb@naver.com>
2022-07-11 22:45:39 +0900

    타입스크립트 기초 - 변수와 함수타입 정의하기 기본타입 - 문자열, 숫자, 배열

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+// js 문자열 선언
+// const str = 'hello';
+
+// ts 문자열 선언
+const str: string = 'hello';
+
+// ts 숫자 선언
+const num: number = 10;
+
+// ts 배열 선언
+const arr: Array<number> = [1,2,3]; // number로된 배열이다.
+const heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10]; // 10이 들어가면 에러가 남
+const items: number[] = [1,2,3] // Array<number> == number[]
