944f98a8cbb6a5d29fa3f1ec894d583384c13445
wjddk0909 <fjddkcb@naver.com>
2022-07-18 20:58:47 +0900

    유니온 타입과 인터섹션 타입의 차이점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -29,13 +29,19 @@ interface Person {
     age: number;
 }
 function askSomeone(someone: Developer | Person) {
-    someone.name = 'abc';
+    // someone.name = 'abc';
     // someone.skill = 'javascript'
 }
+askSomeone({ name: 'Developer', skill: '웹 개발' });
+askSomeone({ name: 'Developer', age: 100 });
+
 function askSomeone2(someone: Developer & Person) {
-    someone.name = 'abc';
-    someone.skill = 'javascript'
+    // someone.name = 'abc';
+    // someone.skill = 'javascript'
 }
+
+askSomeone2({ name: 'Developer', skill: '웹 개발' }) // 에러
+askSomeone2({ name: 'Developer', skill: '웹 개발', age: 100 })
 
 // const test1: string | number | boolean;
 // const test2: string & number & boolean;
