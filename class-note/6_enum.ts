5408370b91e9b38c22a4e716035be40f2b147b99
wjddk0909 <fjddkcb@naver.com>
2022-07-18 21:17:10 +0900

    문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -6,3 +6,12 @@ enum Shoes {
 const myShoes = Shoes.Nike;
 
 console.log(myShoes); // 0 출력
+
+enum Shoes2 {
+    Nike = '나이키',
+    Adidas = '아디다스'
+}
+
+const myShoes2 = Shoes2.Nike;
+
+console.log(myShoes2); // 나이키
