360e007d8cb944e6dd85c091b1dce58f62fa4aa6
wjddk0909 <fjddkcb@naver.com>
2022-07-18 17:09:33 +0900

    함수의 인자를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,18 @@ interface User {
     name: string;
 }
 
+// 변소에 인터페이스 활용
 const ellin: User = {
     age: 33,
     name: 'Ellin'
 }
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+    console.log(user);
+}
+const ja = {
+    name: 'jungah',
+    age: 35
+}
+getUser(ja)
