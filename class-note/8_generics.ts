7b0f3913be42aef3c4cd9d07563c1a1e7cf90c0c
wjddk0909 <fjddkcb@naver.com>
2022-07-21 00:45:30 +0900

    기존 타입 정의 방식과 제네릭의 차이점 - 함수 중복 선언의 단점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -7,10 +7,19 @@
 // logText('hi');
 // logText(true);
 
-function logText<T>(text: T):T {
+// function logText<T>(text: T):T {
+//     console.log(text);
+//     return text;
+// }
+//
+// logText<string>('hi');
+// logText<number>(10);
+
+function logText(text: string) {
     console.log(text);
+    text.split('').reverse().join('');
     return text;
 }
-
-logText<string>('hi');
-logText<number>(10);
+logText('a');
+logText(10); // error
+logText(true) // error
