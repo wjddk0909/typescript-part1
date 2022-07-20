d6e122b2e1eecf7451c3a4812a2781df0bf2f944
wjddk0909 <fjddkcb@naver.com>
2022-07-20 23:29:06 +0900

    클래스 소개

---
+++ class-note/class.js
@@ -0,0 +1,12 @@
+// ES2015 (ES6)
+class Person {
+    // 클래스 로직 -> 인스턴스 만들기
+    constructor(name, age) {
+        console.log('생성 되었습니다.');
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const ja = new Person('ellin', 20); // 생성 되었습니다.
+console.log(ja)
