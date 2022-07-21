0a245578d710c2260a52b6876407e99f68066899
wjddk0909 <fjddkcb@naver.com>
2022-07-21 21:58:51 +0900

    제네릭의 장점과 타입 추론에서의 이점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -15,13 +15,26 @@
 // logText<string>('hi');
 // logText<number>(10);
 
-function logText(text: string | number) {
+// function logText(text: string | number) {
+//     console.log(text);
+//     return text;
+// }
+//
+// logText('a');
+// const a = logText('a');
+// a.split('') // error
+// logText(10);
+// logText(true) // error
+
+function logText<T>(text: T): T {
     console.log(text);
     return text;
 }
 
-logText('a');
-const a = logText('a');
-a.split('') // error
-logText(10);
-// logText(true) // error
+// logText('a')
+// logText(10)
+
+const str = logText<string>('a');
+str.split('') // 에러 없음, str에 마우스 올리면 프리뷰에 strign으로 추론되는것 확인 가능
+const login = logText<boolean>(true);
+
