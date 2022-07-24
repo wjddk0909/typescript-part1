096df08c21bb5f49aa4f6086e9d0dbe5c0c06121
wjddk0909 <fjddkcb@naver.com>
2022-07-24 21:35:42 +0900

    복잡한 구조에서의 타입 추론 방식

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -7,12 +7,29 @@ function getC(c = 10) {
 }
 
 // 타입추론 기본2
+// interface Dropdown<T> {
+//     value: T;
+//     title: string;
+// }
+//
+// let shoppingItem: Dropdown<string> = {
+//     value: 'abc',
+//     title: 'hello'
+// }
+
+// 타입추론 기본3
 interface Dropdown<T> {
     value: T;
     title: string;
 }
+interface DetailedDropdown<K> extends Dropdown<K>{
+    description: string;
+    tag: K;
+}
 
-let shoppingItem: Dropdown<string> = {
-    value: 'abc',
-    title: 'hello'
+let detailedItem: DetailedDropdown<string> = {
+    title: 'abc',
+    description: 'ab',
+    value: 'a',
+    tag: 'b'
 }
